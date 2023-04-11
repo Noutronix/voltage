8840ea19b151777e4dc654763bd74e8981ad4cc7
Noah Deschenes <73668501+Noutronix@users.noreply.github.com>
2023-04-11 16:48:41 -0400

    Deux options distinctes

--- voltage_simulation.py
+++ voltage_simulation.py
@@ -2,8 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import tkinter as tk
+import tkinter.ttk as ttk
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
+import functools as ft
+
+
 
 k = 8.9875517923*(10**9) #coulomb's constant
 Q = 1.60217663*(10**-19) #charge for an electron/proton
@@ -32,10 +36,12 @@ def log_tick_formatter(val, pos=None):
 
 
 class space:
-    def __init__(self):
+    def __init__(self, num):
         np.zeros((50, 50))
-        self.particles = [particle(-1, (-3, 3)), particle(1, (3, 3)), particle(-1, (-3, -3)), particle(1, (3, -3))]
-
+        if num == 0:
+            self.particles = [particle(-1, (-3, 9)), particle(-1, (-3, 3)), particle(1, (3, 9)), particle(1, (3, 3)), particle(-1, (-3, -3)), particle(1, (3, -3)), particle(1, (3, -9)), particle(-1, (-3, -9))]
+        if num == 1:
+            self.particles = [particle(-1, (-6, 9)), particle(-1, (-6, 3)), particle(1, (6, 9)), particle(1, (6, 3)), particle(-1, (-6, -3)), particle(1, (6, -3)), particle(1, (6, -9)), particle(-1, (-6, -9))]
     
     def zline(self, X, Y):
         values = np.zeros((60, 60))
@@ -53,7 +59,7 @@ class particle:
         self.x = location[0]
         self.y = location[1]
 
-def plot():
+def plot(num):
     clean()
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -61,15 +67,8 @@ def plot():
     ax.zaxis.set_major_locator(mticker.MaxNLocator(integer=True))
 
 
-    xline = np.linspace(-16, 16, 60)
-    yline = np.linspace(-16, 16, 60)
-    X, Y = np.meshgrid(xline, yline)
-
-    s = space()
-    z = s.zline(X, Y)
-    Z = np.array([[manual_log(z[n1][n2]) for n2 in range(len(z[n1]))] for n1 in range(len(z))])
-
-    ax.plot_surface(X, Y, Z, cmap=plt.cm.gray)
+    c = coords(num)
+    ax.plot_surface(c[0], c[1], c[2], cmap=plt.cm.gray)
 
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.draw()
@@ -79,11 +78,27 @@ def plot():
     toolbar.update()
     canvas.get_tk_widget().pack()
 
+    return_btn = tk.Button(master=window, command=main, text="Retourner")
+    return_btn.pack()
 
 def main():
     clean()
-    plot_btn = tk.Button(master=window, command=plot, text="plot")
-    plot_btn.pack()
+    plot1_btn = tk.Button(master=window, command=ft.partial(plot, 0), text="Plusieurs charges rapproches")
+    plot1_btn.pack()
+    plot2_btn = tk.Button(master=window, command=ft.partial(plot, 1), text="Plusieurs charges eloignes")
+    plot2_btn.pack()
+
+def coords(num):
+    xline = np.linspace(-16, 16, 60)
+    yline = np.linspace(-16, 16, 60)
+    X, Y = np.meshgrid(xline, yline)
+    s = space(num)
+    z = s.zline(X, Y)
+    Z = np.array([[manual_log(z[n1][n2]) for n2 in range(len(z[n1]))] for n1 in range(len(z))])
+    return (X, Y, Z)
+
+
+
 
 
 
@@ -92,8 +107,8 @@ window = tk.Tk()
 start_btn = tk.Button(master=window, command=main, text="Cliquez ici pour commencer")
 start_btn.pack()
 
-bibliography_btn = tk.Button(master=window, command=bibliography, text="Voir la bibliographie")
-bibliography_btn.pack()
+#bibliography_btn = tk.Button(master=window, command=bibliography, text="Voir la bibliographie")
+#bibliography_btn.pack()
 
 
 
